abee10f1f1367b7bed655e6303d8e5bc9fcb6988
Lacey Merryman <laceymerryman@Laceys-MacBook-Pro.local>
2025-08-26 13:26:08 -1000

    modify initial

--- index.js
+++ index.js
@@ -12,23 +12,33 @@ program
   .command("getQuote")
   .description("Retrieves a random quote")
   .action(async () => {
-    // TODO: Pull a random quote from the quotes.txt file
-    // console log the quote and author
-    // You may style the text with chalk as you wish
+    try {
+      const data = await fs.readFile(QUOTE_FILE, "utf-8");
+      const lines = data.split("/n").filter(line => line.trim() !== "");
+      if (lines.length === 0) {
+        console.log(chalk.yellow("No quotes found."));
+        return;
+      }
+      const randomLine = lines[Math.floor(Math.random() * lines.length)];
+      const [quote, author] = random.Line.split("|");
+      console.log(chalk.green('"${quote.trim()}"'))
+      console.log(chalk.blue('_ ${author ? author.trim() : "Anonymous"}'));
+    } catch (err) {
+      console.error(chalk.red("Error reading quotes file:"), err.message);
+    }
   });
 
 program
   .command("addQuote <quote> [author]")
   .description("adds a quote to the quote file")
   .action(async (quote, author) => {
-    // TODO: Add the quote and author to the quotes.txt file
-    // If no author is provided,
-    // save the author as "Anonymous".
-    // After the quote/author is saved,
-    // alert the user that the quote was added.
-    // You may style the text with chalk as you wish
-    // HINT: You can store both author and quote on the same line using
-    // a separator like pipe | and then using .split() when retrieving
+    try {
+      const lineToAdd = '${quote.trim()}|${(author || "Anonymous").trim()}\n'; 
+      await fs.appendFile(QUOTE_FILE, lineToAdd);
+      console.log(chalk.green("Quote added successfully!"));
+      } catch (err) {
+        console.error(chalk.red("Error writing to quotes file:"), err.message);
+      }
   });
 
 program.parse();
